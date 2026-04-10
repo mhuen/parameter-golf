e0b226e1418372f5dc45aeaacd3b8fc9cdc2ce26
Mirco Huennefeld <mirco@asari.ai>
2026-04-10 02:54:37 -0700

    MultiStreamGPT: turn of logit stream scale by default

--- multi_stream_gpt.py
+++ multi_stream_gpt.py
@@ -328,7 +328,7 @@ class MultiStreamGPT(nn.Module):
         k_shift: bool = True,
         # Output
         logit_softcap: float = 30.0,
-        logit_stream_normalization_factor: float = 10.0,
+        logit_stream_normalization_factor: float = 1.0,
         # Priors
         include_bigram_prior: bool = True,
         include_utf8_prior: bool = True,
